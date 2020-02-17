d6c8ade5321d0c3c2570ac821f6d6898ff68a26b
elainejac <elainejac@github.com>
2020-02-16 23:38:05 -0800

    hw19 upload
    webpage with added case study page

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+(function scrollReveal() {
+window.sr = ScrollReveal({ reset: true });
+ScrollReveal().reveal('.media', { delay: 250 });
+ScrollReveal().reveal('.media', { duration: 500 });
+ScrollReveal().reveal('.media', { delay: 375, reset: true });
+})();
+
+
+//window.sr = ScrollReveal();
+//sr.reveal('.foo');
+//sr.reveal('.bar');
