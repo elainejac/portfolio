93ea120ab1f398b7c3fc1e5de1d5a624c0bd365b
elainejac <elainejac@github.com>
2020-02-23 08:06:39 -0800

    post-review edits
    attempted scrollreveal, edited responsive sizing for tools icons, added meta viewport in head tags to adjust accordinly on smaller devices

--- js/index.js
+++ js/index.js
@@ -1,10 +1,5 @@
 
-(function scrollReveal() {
-window.sr = ScrollReveal({ reset: true });
-ScrollReveal().reveal('.media', { delay: 250 });
-ScrollReveal().reveal('.media', { duration: 500 });
-ScrollReveal().reveal('.media', { delay: 375, reset: true });
-})();
+
 
 
 //window.sr = ScrollReveal();
